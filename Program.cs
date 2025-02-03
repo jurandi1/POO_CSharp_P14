c05333406e395b302626222e8c8fba469478c7af
Jurandi Junior <jurandijr20@gmail.com>
2025-02-02 21:31:05 -0300

    File, FileInfo e IOException

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
-﻿internal class Program
+﻿using System.IO;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        string sourcePath = @"C:\Windows\Temp\file1.txt";
+        string targetPath = @"C:\Windows\Temp\file2.txt";
+
+        try
+        {
+            FileInfo fileInfo = new FileInfo(sourcePath);
+            fileInfo.CopyTo(targetPath);
+
+            string[] lines = File.ReadAllLines(sourcePath);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+        }catch (IOException e)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(e.Message);
+        }
     }
 }
