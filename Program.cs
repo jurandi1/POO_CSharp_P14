e10444c1eb689305676d11cc1341f8c5b3b1c053
Jurandi Junior <jurandijr20@gmail.com>
2025-02-04 16:56:44 -0300

    FileStream e StreamReader

--- Program.cs
+++ Program.cs
@@ -4,6 +4,34 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        // FileStream e StreamReader
+        string path = @"C:\Windows\Temp\file1.txt";
+        StreamReader sr = null;
+        try
+        {
+            sr = File.OpenText(path);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (sr != null) sr.Close();
+        }
+
+
+
+
+
+        //File, FileInfo e IOException
+        /*
         string sourcePath = @"C:\Windows\Temp\file1.txt";
         string targetPath = @"C:\Windows\Temp\file2.txt";
 
@@ -23,5 +51,6 @@ internal class Program
             Console.WriteLine("An error occurred");
             Console.WriteLine(e.Message);
         }
+        */
     }
 }
